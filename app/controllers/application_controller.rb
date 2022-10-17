5f63bed0b3edb9414bbbc3932641c0c147bf5af9
Gustavo Santiago <gustavosantig1@gmail.com>
2022-10-17 13:17:34 -0300

    Apply devise token auth to includes sessions for see events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
   def not_found
     render json: {
       data: {
